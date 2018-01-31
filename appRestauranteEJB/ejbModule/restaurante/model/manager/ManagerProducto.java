a59b4c464a7d080824c8745fbde34f9be76d1aa9
David <davidcad963@gmail.com>
2018-01-31 14:29:39 -0500

    union y complemento de factura venta con la version del menu de navegacion

---
+++ appRestauranteEJB/ejbModule/restaurante/model/manager/ManagerProducto.java
@@ -0,0 +1,136 @@
+package restaurante.model.manager;
+
+import java.util.List;
+import java.math.BigDecimal;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import restaurante.model.entities.TabInvBodega;
+import restaurante.model.entities.TabInvCategoriaProducto;
+import restaurante.model.entities.TabInvProducto;
+import restaurante.model.entities.TabInvProveedor;
+import restaurante.model.entities.TabVtsCliente;
+
+/**
+ * Session Bean implementation class ManagerProducto
+ */
+@Stateless
+@LocalBean
+public class ManagerProducto {
+
+    @PersistenceContext(unitName = "restauranteDS")
+    private EntityManager em;
+	
+    public ManagerProducto() {
+        // TODO Auto-generated constructor stub
+    }
+    public TabInvCategoriaProducto findCategoriaProdByID(int idcategoria) throws Exception {
+		TabInvCategoriaProducto c = em.find(TabInvCategoriaProducto.class, idcategoria);
+		return c;
+	}
+    public List<TabInvCategoriaProducto> findAllCategorias() {
+		Query q;
+		List<TabInvCategoriaProducto> listado;
+		String sentenciaSQL;
+		sentenciaSQL = "SELECT c FROM TabInvCategoriaProducto c ORDER BY c.nombrecategoria";
+		q = em.createQuery(sentenciaSQL);
+		listado = q.getResultList();
+		return listado;
+	}
+  
+    public List<TabInvBodega> findAllBodegas() {
+		Query q;
+		List<TabInvBodega> listado;
+		String sentenciaSQL;
+		sentenciaSQL = "SELECT t FROM TabInvBodega t";
+		q = em.createQuery(sentenciaSQL);
+		listado = q.getResultList();
+		return listado;
+	}
+    public TabInvProducto findProductoByID(int idproducto) throws Exception {
+		TabInvProducto p = em.find(TabInvProducto.class, idproducto);
+		return p;
+	}
+    public TabInvBodega findBodegaByID(int idbodega) throws Exception {
+		TabInvBodega b = em.find(TabInvBodega.class, idbodega);
+		return b;
+	}
+    public List<TabInvProducto> findAllProductos() {
+		Query q;
+		List<TabInvProducto> listado;
+		String sentenciaSQL;
+		sentenciaSQL = "SELECT p FROM TabInvProducto p";
+		q = em.createQuery(sentenciaSQL);
+		listado = q.getResultList();
+		return listado;
+	}
+    
+    
+    public void agregarproducto(Integer idcategoria, Integer idbodega, String nombreproducto, String descripcionproducto, BigDecimal valorcompra ,
+    		BigDecimal valorventa, BigDecimal stock,Boolean estado, String unidadmedida) throws Exception {
+    	TabInvCategoriaProducto cp;
+    	TabInvBodega b;
+    	if (idcategoria == null || idcategoria.equals(0))
+			throw new Exception("Debe especificar la categoria.");
+    	if (idbodega == null || idbodega.equals(0))
+			throw new Exception("Debe especificar la bodega.");
+    	
+		
+		TabInvProducto p = new TabInvProducto();
+		cp=findCategoriaProdByID(idcategoria);
+		b=findBodegaByID(idbodega);
+		
+		p.setTabInvCategoriaProducto(cp);
+		p.setTabInvBodega(b);
+		p.setNombreproducto(nombreproducto);
+		p.setDescripcionproducto(descripcionproducto);
+		p.setValorcompra(valorcompra);
+		p.setValorventa(valorventa);
+		p.setStock(stock);
+		p.setEstadoproducto(estado);
+		p.setUnidadmedida(unidadmedida);				
+		em.persist(p);
+	}
+    public TabInvProducto findproductoByID(Integer idproducto) throws Exception{
+    	TabInvProducto p=em.find(TabInvProducto.class, idproducto);
+    	return p;
+    }
+    public void editarproducto(Integer idproducto,Integer idcategoria, Integer idbodega, String nombreproducto, String descripcionproducto, BigDecimal valorcompra ,
+    		BigDecimal valorventa, BigDecimal stock,Boolean estado, String unidadmedida) throws Exception{
+    	TabInvCategoriaProducto cp=findCategoriaProdByID(idcategoria);
+		TabInvBodega b=findBodegaByID(idbodega);
+    	TabInvProducto p=findproductoByID(idproducto);
+    	if(p == null)
+    		throw new Exception("No existe el producto especificado.");
+    	cp=findCategoriaProdByID(idcategoria);
+		b=findBodegaByID(idbodega);
+		
+		p.setTabInvCategoriaProducto(cp);
+		p.setTabInvBodega(b);
+		p.setNombreproducto(nombreproducto);
+		p.setDescripcionproducto(descripcionproducto);
+		p.setValorcompra(valorcompra);
+		p.setValorventa(valorventa);
+		p.setStock(stock);
+		p.setEstadoproducto(estado);
+		p.setUnidadmedida(unidadmedida);				
+		em.persist(p);
+    	
+    }
+    public List<TabInvProducto> findAllProducto() {
+		Query q;
+		List<TabInvProducto> listado;
+		String sentenciaSQL;
+		sentenciaSQL = "SELECT c FROM TabInvProducto c ORDER BY c.nombreproducto";
+		q = em.createQuery(sentenciaSQL);
+		listado = q.getResultList();
+		return listado;
+	}
+    
+
+     
+
+}
---
+++ appRestauranteWeb/src/restaurante/controller/ControllerProducto.java
@@ -0,0 +1,213 @@
+package restaurante.controller;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+
+import restaurante.model.entities.TabInvProducto;
+import restaurante.model.entities.TabVtsCliente;
+import restaurante.model.entities.TabInvBodega;
+import restaurante.model.entities.TabInvCategoriaProducto;
+import restaurante.model.manager.ManagerProducto;
+import restaurante.view.util.JSFUtil;
+
+@ManagedBean
+@SessionScoped
+public class ControllerProducto {
+	private Integer idproducto;
+	private Integer idcategoria;
+	private Integer idbodega;
+	private String nombreproducto;
+	private String descripcionproducto;
+	private BigDecimal valorcompra;
+	private BigDecimal valorventa;
+	private BigDecimal stock;
+	private Boolean estado;
+	private String unidadmedida;
+	private List<TabInvProducto> lista;
+	
+	@EJB
+	private ManagerProducto managerProducto;
+	
+	
+	@PostConstruct
+	public void iniciar() {
+		lista = managerProducto.findAllProducto();
+	}
+	
+	public List<SelectItem> getListaProductoSI() {
+		List<SelectItem> listadoSI = new ArrayList<SelectItem>();
+		List<TabInvBodega> listadoProdcutos = managerProducto.findAllBodegas();
+
+		for (TabInvBodega c : listadoProdcutos) {
+			SelectItem item = new SelectItem(c.getIdbodega(),c.getNombrebodega());
+			listadoSI.add(item);
+		}
+		return listadoSI;
+	}
+	public List<SelectItem> getListaCategoriaSI() {
+		List<SelectItem> listadoCSI = new ArrayList<SelectItem>();
+		List<TabInvCategoriaProducto> listadoCategoria = managerProducto.findAllCategorias();
+
+		for (TabInvCategoriaProducto c : listadoCategoria) {
+			SelectItem item = new SelectItem(c.getIdcategoria(),c.getNombrecategoria());
+			listadoCSI.add(item);
+		}
+		return listadoCSI;
+	}
+
+	
+	public void agregarProducto() {
+		try {
+			managerProducto.agregarproducto(idcategoria, idbodega, nombreproducto, descripcionproducto, valorcompra, valorventa, stock, estado, unidadmedida);
+			lista = managerProducto.findAllProducto();
+			JSFUtil.crearMensajeInfo("Producto registrado.");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+		idcategoria=null;
+		idbodega=null;
+		nombreproducto="";
+		descripcionproducto="";
+		valorcompra=null;
+		valorventa=null;
+		stock=null;		
+		unidadmedida="";
+	}
+	public void CargarProducto(TabInvProducto producto) {
+		idproducto = producto.getIdproducto();
+		idcategoria=producto.getTabInvCategoriaProducto().getIdcategoria();
+		idbodega=producto.getTabInvBodega().getIdbodega();
+		nombreproducto=producto.getNombreproducto();
+		descripcionproducto=producto.getDescripcionproducto();
+		valorcompra=producto.getValorcompra();
+		valorventa=producto.getValorventa();		
+		stock=producto.getStock();
+		unidadmedida=producto.getUnidadmedida();
+		estado =producto.getEstadoproducto();				
+	}
+	
+	public void EditarProveedor() {
+		try {
+			managerProducto.editarproducto(idproducto, idcategoria, idbodega, nombreproducto, descripcionproducto, valorcompra, valorventa, stock, estado, unidadmedida);
+
+			lista = managerProducto.findAllProducto();
+			JSFUtil.crearMensajeInfo("Producto con nombre" + nombreproducto + " editado correctamente.");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+		idcategoria=null;
+		idbodega=null;
+		nombreproducto="";
+		descripcionproducto="";
+		valorcompra=null;
+		valorventa=null;
+		stock=null;		
+		unidadmedida="";
+	}
+	
+
+	public Integer getIdproducto() {
+		return idproducto;
+	}
+
+	public void setIdproducto(Integer idproducto) {
+		this.idproducto = idproducto;
+	}
+
+	public Integer getIdcategoria() {
+		return idcategoria;
+	}
+
+	public void setIdcategoria(Integer idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+
+	public Integer getIdbodega() {
+		return idbodega;
+	}
+
+	public void setIdbodega(Integer idbodega) {
+		this.idbodega = idbodega;
+	}
+
+	public String getNombreproducto() {
+		return nombreproducto;
+	}
+
+	public void setNombreproducto(String nombreproducto) {
+		this.nombreproducto = nombreproducto;
+	}
+
+	public String getDescripcionproducto() {
+		return descripcionproducto;
+	}
+
+	public void setDescripcionproducto(String descripcionproducto) {
+		this.descripcionproducto = descripcionproducto;
+	}
+
+	public BigDecimal getValorcompra() {
+		return valorcompra;
+	}
+
+	public void setValorcompra(BigDecimal valorcompra) {
+		this.valorcompra = valorcompra;
+	}
+
+	public BigDecimal getValorventa() {
+		return valorventa;
+	}
+
+	public void setValorventa(BigDecimal valorventa) {
+		this.valorventa = valorventa;
+	}
+
+	public BigDecimal getStock() {
+		return stock;
+	}
+
+	public void setStock(BigDecimal stock) {
+		this.stock = stock;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public String getUnidadmedida() {
+		return unidadmedida;
+	}
+
+	public void setUnidadmedida(String unidadmedida) {
+		this.unidadmedida = unidadmedida;
+	}
+
+	public List<TabInvProducto> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<TabInvProducto> lista) {
+		this.lista = lista;
+	}
+
+	public ManagerProducto getManagerProducto() {
+		return managerProducto;
+	}
+
+	public void setManagerProducto(ManagerProducto managerProducto) {
+		this.managerProducto = managerProducto;
+	}	
+}
