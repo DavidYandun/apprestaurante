d404a4757f8664c6b295686adebc5e1e7e41d28a
David <davidcad963@gmail.com>
2018-02-01 21:31:39 -0500

    Union master 1 con master 2 primera version

--- appRestauranteWeb/src/restaurante/controller/ControllerUsuario.java
+++ appRestauranteWeb/src/restaurante/controller/ControllerUsuario.java
@@ -1,12 +1,16 @@
 package restaurante.controller;
 
+import java.io.IOException;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import restaurante.model.entities.TabLogUsuario;
 import restaurante.model.manager.ManagerLogin;
+import restaurante.model.util.ModelUtil;
 import restaurante.view.util.JSFUtil;
 
 @ManagedBean
@@ -30,15 +34,62 @@ public class ControllerUsuario {
 			u = managerUsuarios.findUsuario(nombreUsuario, correoUsuario);
 			JSFUtil.crearMensajeInfo("Login correcto");
 			// verificamos si el acceso es con admin:
-			if (u.getTabLogTipoUsuario().getTipousuario().equals("admin")) {
-				contex.getExternalContext().redirect("Caja diaria/tipotransaccion.xhtml");
-			}
-			contex.getExternalContext().redirect("Punto de venta/cliente.xhtml");
+			if (u.getTabLogTipoUsuario().getTipousuario().equals("admin"))
+				contex.getExternalContext().redirect("admin/cajadiaria/tipotransaccion.xhtml");
+			if (u.getTabLogTipoUsuario().getTipousuario().equals("bodeguero"))
+				contex.getExternalContext().redirect("bodeguero/inventario/bodega.xhtml");
+			if (u.getTabLogTipoUsuario().getTipousuario().equals("cajero"))
+				contex.getExternalContext().redirect("cajero/puntodeventa/pedido.xhtml");
 		} catch (Exception e) {
 			JSFUtil.crearMensajeError(e.getMessage());
 			e.printStackTrace();
 		}
 		return "";
+	}
+
+	public String actionSalir() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/login?faces-redirect=true";
+	}
+
+	public void actionComprobarLogin() {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		try {
+			String path = ec.getRequestPathInfo();
+			System.out.println("getRequestContextPath(): " + ec.getRequestContextPath());
+			System.out.println("getRequestPathInfo(): " + path);
+			System.out.println("Id usuario: " + u.getIdusuario());
+			if (path.equals("/login.xhtml"))
+				return;
+			if (ModelUtil.isEmpty(u.getIdusuario() + ""))
+				ec.redirect(ec.getRequestContextPath() + "/faces/login.xhtml");
+			if (!respuesta) {
+				ec.redirect(ec.getRequestContextPath() + "/faces/login.xhtml");
+			} else {
+				// si hizo login, verificamos que acceda a paginas permitidas:
+				if (u.getTabLogTipoUsuario().getTipousuario().equals("admin")) {
+					if (!path.contains("/admin/"))
+						ec.redirect(ec.getRequestContextPath() + "/faces/login.xhtml");
+					else
+						return;
+				}
+				// caso contrario es un blogger:
+				if (u.getTabLogTipoUsuario().getTipousuario().equals("bodeguero")) {
+					if (!path.contains("/bodeguero/"))
+						ec.redirect(ec.getRequestContextPath() + "/faces/login.xhtml");
+					else
+						return;
+				}
+				if (u.getTabLogTipoUsuario().getTipousuario().equals("cajero")) {
+					if (!path.contains("/cajero/"))
+						ec.redirect(ec.getRequestContextPath() + "/faces/login.xhtml");
+					else
+						return;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getIdUsuario() {
@@ -91,10 +142,6 @@ public class ControllerUsuario {
 
 	public boolean isRespuesta() {
 		return respuesta;
-	}
-
-	public void setRespuesta(boolean respuesta) {
-		this.respuesta = respuesta;
 	}
 
 }
